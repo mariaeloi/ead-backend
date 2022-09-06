8112683098066862fcf842ded3dddf1833021b50
mariaeloi <mariaeduardaeloi@outlook.com>
2022-09-05 21:55:15 -0300

    Adiciona validação para Username e Email em uso

--- Services/User/UserService.cs
+++ Services/User/UserService.cs
@@ -38,7 +38,7 @@ public class UserService : IService<User>
     public User GetById(long id)
     {
         User user = _uow.UserRepository.FindById(id);
-        if(user == null)
+        if (user == null)
             throw new NotFoundException("Usuário não encontrado");
 
         return user;
@@ -47,13 +47,15 @@ public class UserService : IService<User>
     public User Update(User user)
     {
         User loggedInUser = _auth.LoggedInUser;
-        if(loggedInUser.Id != user.Id)
+        if (loggedInUser.Id != user.Id)
             throw new AccessDeniedException("Você não tem permissão para atualizar este usuário");
 
         this.Validate(user);
 
-        if(!user.Password.Equals(loggedInUser.Password))
+        if (!user.Password.Equals(loggedInUser.Password))
             user.Password = Crypter.MD5.Crypt(user.Password);
+        user.CreatedOn = loggedInUser.CreatedOn;
+        user.UpdatedOn = DateTime.Now;
 
         return _uow.UserRepository.Update(user);
     }
@@ -61,18 +63,30 @@ public class UserService : IService<User>
     public void Delete(long id)
     {
         User loggedInUser = _auth.LoggedInUser;
-        if(loggedInUser.Id != id)
+        if (loggedInUser.Id != id)
             throw new AccessDeniedException("Você não tem permissão para remover este usuário");
-    
+
         _uow.UserRepository.DeleteById(id);
     }
 
     public void Validate(User user)
     {
-        var validation = _validator.Validate(user);
-        if(!validation.IsValid)
-            throw new ValidationException(validation.ToDictionary());
+        var errors = _validator.Validate(user).ToDictionary();
 
-        // TODO validar se já existe Username e Email iguais
+        if (!errors.ContainsKey("Username"))
+        {
+            User userExist = _uow.UserRepository.FindOne(u => u.Id != user.Id && u.Username.ToLower().Equals(user.Username.ToLower()));
+            if (userExist != null)
+                errors.Add("Username", new string[] { "Este nome de usário encontra-se em uso" });
+        }
+        if (!errors.ContainsKey("Email"))
+        {
+            User userExist = _uow.UserRepository.FindOne(u => u.Id != user.Id && u.Email.ToLower().Equals(user.Email.ToLower()));
+            if (userExist != null)
+                errors.Add("Email", new string[] { "Este e-mail encontra-se em uso" });
+        }
+
+        if(errors.Count > 0)
+            throw new ValidationException(errors);
     }
 }
