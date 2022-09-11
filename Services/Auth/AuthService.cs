297bda9a2ccd4f441af48b61b807284b26a0e715
mariaeloi <mariaeduardaeloi@outlook.com>
2022-09-11 15:32:02 -0300

    Salva log ao realizar login

--- Services/Auth/AuthService.cs
+++ Services/Auth/AuthService.cs
@@ -1,4 +1,5 @@
 using CryptSharp;
+using Domain.Constants;
 using Domain.Entities;
 using Infra.Repositories.Interfaces;
 using Services.Exceptions;
@@ -8,13 +9,16 @@ namespace Services;
 
 public class AuthService
 {
+    private const string ENTITY_NAME = EntityNameConstant.User;
     private readonly IUnitOfWork _uow;
     private readonly ITokenService _tokenService;
+    private readonly ILogService _logger;
 
-    public AuthService(IUnitOfWork uow, ITokenService tokenService)
+    public AuthService(IUnitOfWork uow, ITokenService tokenService, ILogService logger)
     {
         this._uow = uow;
         this._tokenService = tokenService;
+        this._logger = logger;
     }
 
     public dynamic Authenticate(User user)
@@ -27,6 +31,7 @@ public class AuthService
             throw new BadCredentialsException();
 
         var token = _tokenService.GenerateToken(userExist);
+        _logger.Log(ActionConstant.Login, ENTITY_NAME, userExist.Id);
         return new {
             user = userExist,
             token = token
